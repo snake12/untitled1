97f3ef840e57f1d5f0f6c1280d3d65c38f21011e
Andrii Tsybenko <andrii_tsybenko@epam.com>
2022-01-12 14:32:33 +0200

    Test Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("222");
+        System.out.println("444");
     }
 
 }
